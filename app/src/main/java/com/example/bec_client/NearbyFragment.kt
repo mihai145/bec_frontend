ed3fb6b1d2e3d7331b391ac163d28ce611f7069e
imcatag <imcatag@gmail.com>
2023-04-01 18:07:17 +0300

    Map of cinemas seems to work, will need a valid GOOGLE_MAPS_API_KEY set up in local.properties to actually run. Ask catag for key or create one yourself.

--- app/src/main/java/com/example/bec_client/NearbyFragment.kt
+++ app/src/main/java/com/example/bec_client/NearbyFragment.kt
@@ -1,26 +1,139 @@
 package com.example.bec_client
 
+import android.Manifest
+import android.content.ContentValues.TAG
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.gson.Gson
+import java.net.HttpURLConnection
+import java.net.URL
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.concurrent.Executors
 
 /**
  * A simple [Fragment] subclass.
  * Use the [NearbyFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+// class with fields for each field in the response from places api
+
+data class TheatersResponse(
+    val results: MutableList<Theater>
+)
+
+data class Theater(
+    val name: String,
+    val vicinity: String,
+    val geometry: Geometry
+)
+data class Geometry(
+    val location: Location
+)
+data class Location(
+    val lat: Double,
+    val lng: Double
+)
+
 class NearbyFragment : Fragment() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
 
+
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_nearby, container, false)
+
+        var theaters = TheatersResponse(mutableListOf())
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        val loc = fusedLocationClient.lastLocation
+        loc.addOnSuccessListener {
+            if (it != null) {
+                // get list of movie theaters
+                val executor = Executors.newSingleThreadExecutor()
+                executor.execute{
+                    try{
+                        val str = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=cinema&location=${it.latitude},${it.longitude}&radius=5000&key=${BuildConfig.GOOGLE_MAPS_API_KEY}"
+                        val connection = URL(str).openConnection() as HttpURLConnection
+                        connection.requestMethod = "GET"
+                        connection.doOutput = true
+                        connection.connectTimeout = 5000
+                        connection.readTimeout = 5000
+                        val responseCode = connection.responseCode
+                        if(responseCode == HttpURLConnection.HTTP_OK) {
+                            val br = BufferedReader(InputStreamReader(connection.inputStream))
+                            var wholeResponse = ""
+                            var line = br.readLine()
+                            while (line != null) {
+                                wholeResponse += line
+                                line = br.readLine()
+                            }
+                            // parse response into list of movie theaters
+                            val gson = Gson()
+                            theaters = gson.fromJson(wholeResponse, TheatersResponse::class.java)
+//                            Log.d(TAG, "Theaters: $theaters")
+                        } else {
+                            Log.d(TAG, "Error: $responseCode")
+                        }
+                    }
+                    catch(e: Exception){
+                        Log.d(TAG, "Error: $e")
+                    }
+                }
+                executor.shutdown()
+                executor.awaitTermination(5, java.util.concurrent.TimeUnit.SECONDS)
+            }
+        }
+        // unused movie icon
+        // val movieIcon: BitmapDescriptor = BitmapDescriptorFactory.fromResource(R.drawable.baseline_local_movies_24)
+        val supportMapFragment = childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+        supportMapFragment.getMapAsync {
+            // center map on current location
+            if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                it.isMyLocationEnabled = true
+                fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                    if (location != null) {
+
+                        val latLng = LatLng(location.latitude, location.longitude)
+                        it.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14f))
+                        Log.d(TAG, "Theaters: $theaters")
+                        theaters.results.forEach{theater ->
+                            val theaterLatLng = LatLng(theater.geometry.location.lat, theater.geometry.location.lng)
+//                            Log.d(TAG, "Theater: $theater")
+                            val marker = it.addMarker(MarkerOptions().position(theaterLatLng).title(theater.name).snippet(theater.vicinity))
+                            marker?.tag = theater
+                        }
+                    }
+                }
+            } else {
+                ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            }
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nearby, container, false)
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+//        locationManager.removeUpdates(locationListener)
     }
 }
