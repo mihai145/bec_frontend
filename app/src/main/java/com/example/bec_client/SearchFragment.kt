4f1476487ac6ae6bbe4d37016f52535d969fb8f3
AirineiAndrei <acairinei17@gmail.com>
2023-04-02 18:52:10 +0300

    Movie Recycler viewer

--- app/src/main/java/com/example/bec_client/SearchFragment.kt
+++ app/src/main/java/com/example/bec_client/SearchFragment.kt
@@ -2,14 +2,18 @@ package com.example.bec_client
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bec_client.adapter.MovieRecyclerAdapter
 import com.example.restapi.home.viewmodel.SearchViewModel
+
 
 /**
  * A simple [Fragment] subclass.
@@ -18,12 +22,14 @@ import com.example.restapi.home.viewmodel.SearchViewModel
  */
 class SearchFragment : Fragment() {
     private lateinit var searchViewModel:SearchViewModel
+    private lateinit var movieAdapter: MovieRecyclerAdapter
+
     private fun search(query: String) {
         searchViewModel.searchMoviesByName(query)
         searchViewModel.searchedMoviesLiveData?.observe(this, Observer {
             if (it!=null){
-                it.results.forEach{x -> Log.d("Debug",x.toString())}
-
+                movieAdapter.submitList(it.results)
+                Log.d("Debug",it.toString())
             }else{
                 Log.d("DEBUG: ", "a crapat")
             }
@@ -39,6 +45,12 @@ class SearchFragment : Fragment() {
     ): View? {
         val mView : View = inflater.inflate(R.layout.fragment_search, container,false)
         val searchView : SearchView = mView.findViewById(R.id.SearchView);
+        val recyclerView : RecyclerView = mView.findViewById(R.id.recycler_view)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL ,false)
+            movieAdapter = MovieRecyclerAdapter()
+            adapter = movieAdapter
+        }
 
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
---
+++ app/src/main/java/com/example/bec_client/adapter/MovieRecyclerAdapter.kt
@@ -0,0 +1,73 @@
+package com.example.bec_client.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.bec_client.R
+import com.example.restapi.home.data.model.MovieModel
+import kotlin.collections.ArrayList
+
+
+class MovieRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>()
+{
+    private var items: List<MovieModel> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder) {
+
+            is ViewHolder -> {
+                holder.bind(items.get(position))
+            }
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun submitList(List: List<MovieModel>){
+        Log.d("DEBUG in adapter",List.toString())
+        items = List
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder
+    constructor(
+        itemView: View
+    ): RecyclerView.ViewHolder(itemView){
+
+        val image = itemView.findViewById<ImageView>(R.id.image)
+        val title = itemView.findViewById<TextView>(R.id.title)
+        val body = itemView.findViewById<TextView>(R.id.body)
+
+        fun bind(movie: MovieModel){
+            Log.d("Binding",movie.toString())
+            TODO("this bad boy broken no idea why")
+//            val requestOptions = RequestOptions()
+//                .placeholder(R.drawable.ic_launcher_background)
+//                .error(R.drawable.ic_launcher_background)
+//
+//            Glide.with(itemView.context)
+//                .applyDefaultRequestOptions(requestOptions)
+//                .load(movie.posterPath)
+//                .into(image)
+            title.setText(movie.title)
+            body.setText(movie.overview)
+        }
+
+    }
+
+}
