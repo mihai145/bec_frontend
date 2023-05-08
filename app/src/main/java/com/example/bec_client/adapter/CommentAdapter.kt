23f463c08fa52b8a2ade08bd6ca07fe8d3fcf525
dimi999 <dimitriu.andrei@yahoo.com>
2023-05-08 13:55:30 +0300

    Implement live data for comment adatper

--- app/src/main/java/com/example/bec_client/adapter/CommentAdapter.kt
+++ app/src/main/java/com/example/bec_client/adapter/CommentAdapter.kt
@@ -10,10 +10,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.ToggleButton
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -25,7 +22,6 @@ import com.example.restapi.home.data.model.request.CommentInfoModel
 import com.example.restapi.home.data.model.request.CommentLikedModel
 import com.example.restapi.home.data.model.response.LikeResponseModel
 import com.example.restapi.home.data.model.response.SimpleResponseModel
-import com.example.restapi.home.viewmodel.SearchViewModel
 import com.example.restapi.network.ApiClient
 import com.example.restapi.network.ApiInterface
 import retrofit2.Call
@@ -37,10 +33,11 @@ class CommentAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private var items: ArrayList<CardModel> = ArrayList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val lifeCycleOwner = parent.context as LifecycleOwner
         return ViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.comment_layout, parent, false),
             parent.context,
-
+            lifeCycleOwner
         )
     }
 
@@ -72,7 +69,7 @@ class CommentAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         notifyDataSetChanged()
     }
 
-    class ViewHolder(itemView: View, context: Context) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View, context: Context, lifeCycleOwner: LifecycleOwner) : RecyclerView.ViewHolder(itemView) {
         val image: ImageView
         val title: TextView
         val body: TextView
@@ -81,6 +78,7 @@ class CommentAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         val apiInterface: ApiInterface? = ApiClient.getApiClient().create(ApiInterface::class.java)
         val applicationContext : Context
         var id : Long
+        val lifecycleOwner : LifecycleOwner
 
         init {
             image = itemView.findViewById(R.id.image)
@@ -90,6 +88,7 @@ class CommentAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
             likesCount = itemView.findViewById(R.id.likesCount)
             applicationContext = context
             id = 0
+            lifecycleOwner = lifeCycleOwner
         }
 
         fun wasLikedComment(commentId: Long, userId: Long): LiveData<Int> {
@@ -152,29 +151,28 @@ class CommentAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
             val requestOptions = RequestOptions()
                 .placeholder(R.drawable.ic_launcher_background)
                 .error(R.drawable.ic_launcher_background)
-            val userId = card.userId
             var wasLiked :  LiveData<Int>? = null
             var likes : LiveData<Int>? = null
-            if(userId != null) {
-                wasLiked = wasLikedComment(id, userId)
-                if(wasLiked != null && wasLiked != null) {
-                    likeButton.isChecked = true
-                }
+            wasLiked = wasLikedComment(id, MainActivity.userId?.toLong() ?: -1L)
+            likes = getLikesComment(id)
 
-                likes = getLikesComment(id)
-                if(likes != null) {
-                    likesCount.text = "$likes Likes"
-                }
-            }
 
-//            wasLiked?.observe(viewLifecycleOwner, Observer {
-//                Log.d("Was Liked", wasLiked.toString())
-//                if (it != null) {
-//                    if (it == 1)
-//                        likeButton.isChecked = true
-//                    Log.d("Debug Liked", it.toString())
-//                }
-//            })
+            wasLiked?.observe(lifecycleOwner, Observer {
+                Log.d("Was Liked", wasLiked.toString())
+                if (it != null) {
+                    if (it == 1)
+                        likeButton.isChecked = true
+                    Log.d("Debug Liked", it.toString())
+                }
+            })
+            likes?.observe(lifecycleOwner, Observer {
+                Log.d("Likes", it.toString())
+                if (it != null) {
+                    likesCount.text = "$it Likes"
+                    Log.d("Debug Likes", it.toString())
+                }
+            })
+
 
             if (card.imagePath != null) {
                 Glide.with(itemView.context)
@@ -199,11 +197,8 @@ class CommentAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
                                 if (response.code() == 202 && res != null && res.ok) {
                                     Toast.makeText(applicationContext, "Comment was liked", Toast.LENGTH_SHORT)
                                         .show()
-                                    val likes = getLikesComment(id).value
+                                    likes = getLikesComment(id)
                                     Log.d("New number likes", likes.toString())
-                                    if(likes != null) {
-                                        likesCount.text = "$likes Likes"
-                                    }
                                 } else {
                                     Toast.makeText(
                                         applicationContext,
@@ -232,10 +227,7 @@ class CommentAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
                                 if (response.code() == 202 && res != null && res.ok) {
                                     Toast.makeText(applicationContext, "Like was deleted", Toast.LENGTH_SHORT)
                                         .show()
-                                    val likes = getLikesComment(id).value
-                                    if(likes != null) {
-                                        likesCount.text = "$likes Likes"
-                                    }
+                                    likes = getLikesComment(id)
                                 } else {
                                     Toast.makeText(
                                         applicationContext,
